b91287fe1e7ed2c1be7a9236c2918fc9e87fa469
omonk <ollie@olliemonk.com>
2017-12-16 15:35:19 +0000

    remove imdb web scraping, update api function to set a request range, write range to file

--- index.js
+++ index.js
@@ -6,42 +6,69 @@ const fetch = require("node-fetch");
 
 let current = 1;
 const max = 7500000;
+const range = 50;
+
+// const fetchIMDB = (index) => {
+//     return fetch(`http://www.imdb.com/title/${id(index)}`)
+//         .then(res => res.text())
+//         .then(json => {
+//             const start = json.indexOf("\">Budget:<");
+//             const sample = json.slice(start + 14, start + 75).trim().replace(/ \n/g, "");
+//             const budget = /^[$]\d+(?:[.,]\d+)*/g.exec(sample);
+//             return budget || "";
+//         });
+// };
 
 const id = (i) => {
     const str = "tt" + leftPad(i, 7, 0);
     return str;
 };
 
-const fetchIMDB = (index) => {
-    return fetch(`http://www.imdb.com/title/${id(index)}`)
-        .then(res => res.text())
-        .then(json => {
-            const start = json.indexOf("\">Budget:<");
-            const sample = json.slice(start + 14, start + 75).trim().replace(/ \n/g, "");
-            const budget = /^[$]\d+(?:[.,]\d+)*/g.exec(sample);
-            return budget || "";
-        });
-};
-
 const fetchAPI = (index) => {
-    return imdb.getById(`${id(index)}`, {apiKey: "15b45eec"});
+    console.time(`${index}`);
+    return imdb.getReq({
+        id: `${id(index)}`,
+        opts: {
+            apiKey: "f8b951c9",
+            timeout: 2000
+        }
+    }).then((res) => {
+        console.timeEnd(`${index}`);
+        return res;
+    }).catch(err => {
+        console.log(`REQEUST TIMEOUT: ${index}`);
+        return {
+            id: index,
+            message: "timeout",
+        };
+    });
 };
 
-function getMovieInfo(i) {
-    console.log(id(i));
-    return Promise.all([
-        fetchIMDB(i),
-        fetchAPI(i),
-    ]).then(response  => {
-        const movie = Object.assign({}, response[1], {budget: response[0]});
-        new Promise((resolve,reject)=> {
-            fs.writeFile(`movies${i}.json`, stringify(movie), (err) => {
-                if (err) reject(err);
-                resolve(movie);
+function getMovieInfo(index) {
+    return Promise.all(
+        Array.from({ length: range }).map((v, i) => fetchAPI(i + index))
+    )
+        .then(response  => {
+            const movie = response;
+            new Promise((resolve,reject)=> {
+                // console.log(`${movie.title}`);
+                fs.writeFile(`movies${index}.json`, stringify(movie), (err) => {
+                    if (err) reject(err);
+                    resolve(movie);
+                });
             });
-        });
-        getMovieInfo(i + 1);
-    }).catch(err => console.log(err));
+            getMovieInfo(index + range);
+        }).catch(err => console.log(err));
 }
 
-getMovieInfo(1);
+getMovieInfo(current);
+
+// function requestLogger(httpModule){
+//     var original = httpModule.request;
+//     httpModule.request = function(options, callback){
+//         console.log(options.href||options.proto+"://"+options.host+options.path, options.method);
+//         return original(options, callback);
+//     };
+// }
+
+// requestLogger(require("http"));
